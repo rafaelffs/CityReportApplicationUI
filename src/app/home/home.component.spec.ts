83af209d1d19213319d1c2f986a47e7273d5284e
Rafael <rafael.ffs@hotmail.com>
2021-07-31 18:02:36 +0100

    Tests update

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,30 +1,32 @@
 import { HttpClient } from "@angular/common/http";
 import { HomeComponent } from "./home.component";
 import { Location } from '../../model/location';
+import { TestBed } from "@angular/core/testing";
+import { WeatherAPIService } from "../services/weatherapiservice";
 
 
 describe('home', () => {
-  //it('Should return empty list if not cities are loaded', () => {
-  //  var component: HomeComponent = new HomeComponent(new HttpClient(null), location.origin);
-  //  expect(component.cityList).toBeUndefined();
-  //})
+  it('Should return empty list if not cities are loaded', () => {
+    var component: HomeComponent = new HomeComponent(new WeatherAPIService(null, null));
+    expect(component.cityList).toBeUndefined();
+  })
 
-  //it('Should not be empty if a list city is loaded', () => {
-  //  var component: HomeComponent = new HomeComponent(new HttpClient(null), location.origin);
-  //  component.cityList = ["City1", "City2"];
-  //  expect(component.cityList.length).toBeGreaterThan(1);
-  //})
+  it('Should not be empty if a list city is loaded', () => {
+    var component: HomeComponent = new HomeComponent(new WeatherAPIService(null, null));
+    component.cityList = ["City1", "City2"];
+    expect(component.cityList.length).toBeGreaterThan(1);
+  })
 
-  //it('Should return empty list if not locations are loaded', () => {
-  //  var component: HomeComponent = new HomeComponent(new HttpClient(null), location.origin);
-  //  expect(component.location).toBeUndefined();
-  //})
+  it('Should return empty list if not locations are loaded', () => {
+    var component: HomeComponent = new HomeComponent(new WeatherAPIService(null, null));
+    expect(component.location).toBeUndefined();
+  })
 
-  //it('Should not be empty if a location is loaded', () => {
-  //  var component: HomeComponent = new HomeComponent(new HttpClient(null), location.origin);
-  //  component.location = new Location();
-  //  expect(component.location).toBeDefined();
-  //})
+  it('Should not be empty if a location is loaded', () => {
+    var component: HomeComponent = new HomeComponent(new WeatherAPIService(null, null));
+    component.location = new Location();
+    expect(component.location).toBeDefined();
+  })
 
 });
 
